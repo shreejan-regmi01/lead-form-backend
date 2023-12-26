06d1017ff3a3fc0d3ed774a66212e9a84b273e6b
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-12-26 15:17:04 +0545

    updated term to use enum

--- src/applications/dto/create-application.dto.ts
+++ src/applications/dto/create-application.dto.ts
@@ -1,6 +1,20 @@
 import { Transform, Type } from 'class-transformer';
-import { IsBoolean, IsNumber, IsString, ValidateNested } from 'class-validator';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 import { VehicleDto } from './vehicle.dto';
+
+enum Term {
+  OneYear = '1 year',
+  TwoYears = '2 years',
+  ThreeYears = '3 years',
+  FourYears = '4 years',
+  FiveYears = '5 years',
+}
 
 export class CreateApplicationDto {
   @Transform(({ value }) => Number(value))
@@ -11,8 +25,8 @@ export class CreateApplicationDto {
   @IsNumber()
   deposit: number;
 
-  @IsString()
-  term: string;
+  @IsEnum(Term)
+  term: Term;
 
   @Transform(({ value }) => Number(value))
   @IsNumber()
@@ -30,6 +44,7 @@ export class CreateApplicationDto {
   @IsString()
   usage: string;
 
+  //https://stackoverflow.com/questions/53650528/validate-nested-objects-using-class-validator-in-nest-js-controller/53685045#53685045
   @ValidateNested()
   @Type(() => VehicleDto)
   vehicle: VehicleDto;
